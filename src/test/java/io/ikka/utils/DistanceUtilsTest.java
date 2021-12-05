25f0d6502c84969489ef68e6f4b17221d20d79e8
ioikka <belgampaul@gmail.com>
2021-12-06 00:39:18 +0300

    IOIKKA-1 Added distance calculator between 2 coordinates.

---
+++ src/test/java/io/ikka/utils/DistanceUtilsTest.java
@@ -0,0 +1,15 @@
+package io.ikka.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DistanceUtilsTest {
+
+    @Test
+    void distance() {
+        assertEquals(
+                2093712.3781514147,
+                DistanceUtils.distance(56.19, 52.23, 36.33, 4.54));
+    }
+}
